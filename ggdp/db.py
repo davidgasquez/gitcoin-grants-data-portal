a04f49a5f08750fc6c7b89bbce96e63ffca01c2b
David Gasquez <davidgasquez@gmail.com>
2023-10-25 19:19:26 +0200

    chore: 🎨 ruff

--- ggdp/db.py
+++ ggdp/db.py
@@ -1,8 +1,10 @@
-import duckdb
 import os
+
+import duckdb
 
 DATA_DIR = os.getenv("DATA_DIR", "../data")
 con = duckdb.connect(database=f"{DATA_DIR}/dbt.duckdb", read_only=True)
+
 
 def query(sql):
     return con.sql(sql)
