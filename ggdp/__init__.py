f3411b00efc019aff5430fe8ddbd265098e54cfb
DistributedDoge <49067954+DistributedDoge@users.noreply.github.com>
2024-01-15 17:19:59 +0100

    feat: ✨ covalent resource and asset (#27)
    * feat: ⚡️ add covalent resource and asset for eth project registry
    
    * Update .github/workflows/ci.yml
    
    Co-authored-by: David Gasquez <davidgasquez@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: David Gasquez <davidgasquez@gmail.com>
    
    * rename missed covalent_api
    
    ---------
    
    Co-authored-by: David Gasquez <davidgasquez@gmail.com>

--- ggdp/__init__.py
+++ ggdp/__init__.py
@@ -1,11 +1,13 @@
 import os
 
-from dagster import Definitions, load_assets_from_modules, define_asset_job
+
+from dagster import Definitions, load_assets_from_modules, define_asset_job, EnvVar
 from dagster_dbt import dbt_cli_resource, load_assets_from_dbt_project
 from dagster_duckdb import DuckDBResource
 from dagster_duckdb_pandas import DuckDBPandasIOManager
 
 from . import assets
+from . import resources as res
 from . import ops
 
 DBT_PROJECT_DIR = os.path.dirname(os.path.abspath(__file__)) + "/../dbt/"
@@ -18,6 +20,7 @@ dbt_assets = load_assets_from_dbt_project(DBT_PROJECT_DIR, DBT_PROJECT_DIR)
 all_assets = load_assets_from_modules([assets])
 
 resources = {
+    "covalent_api": res.CovalentAPIResource(API_KEY=EnvVar("COVALENT_API_KEY")),
     "dbt": dbt_resource,
     "duckdb": DuckDBResource(database="data/local.duckdb"),
     "io_manager": DuckDBPandasIOManager(
