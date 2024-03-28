c23227ba7a7e72c1aa6803a08806c562002c3b5b
DistributedDoge <49067954+DistributedDoge@users.noreply.github.com>
2024-03-28 12:34:49 +0000

    create hypercerts asset and model

---
+++ dbt/models/hypercert_claims.sql
@@ -0,0 +1,16 @@
+with source as (
+    select * from {{ source('public', 'raw_hypercert_claims') }}
+),
+
+renamed as (
+    select
+        id,
+        tokenID as token_id,
+        lower(contract) as contract,
+        uri as uri,
+        CAST(totalUnits AS BIGINT) as total_units,
+        lower(creator) as creator
+    from source
+)
+
+select * from renamed
