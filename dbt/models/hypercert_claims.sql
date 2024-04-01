826f33fb440adcc8efa6f47dd01f72ed88a66bbb
DistributedDoge <49067954+DistributedDoge@users.noreply.github.com>
2024-04-01 11:16:30 +0200

    Rename `uri` to `ipfs_cid`

--- dbt/models/hypercert_claims.sql
+++ dbt/models/hypercert_claims.sql
@@ -7,7 +7,7 @@ renamed as (
         id,
         tokenID as token_id,
         lower(contract) as contract,
-        uri as uri,
+        uri as ipfs_cid,
         CAST(totalUnits AS BIGINT) as total_units,
         lower(creator) as creator
     from source
